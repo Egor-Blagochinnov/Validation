b5f1ee4792d7a1d2d515b3a2d93fed66d241aa85
Egor Blagochinnov <blagochinnov.e.d@gmail.com>
2020-12-16 12:33:38 +0300

    init

---
+++ app/src/main/java/com/egorblagochinnov/validation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.egorblagochinnov.validation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.egorblagochinnov.validation.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
