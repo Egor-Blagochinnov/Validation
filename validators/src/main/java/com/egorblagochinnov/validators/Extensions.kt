0ffdbbb7d6b725ce206cdef58ce30577c4565ce2
Egor Blagochinnov <blagochinnov.e.d@gmail.com>
2021-04-24 19:42:33 +0300

    add validation Extensions

--- validators/src/main/java/com/egorblagochinnov/validators/Extensions.kt
+++ validators/src/main/java/com/egorblagochinnov/validators/Extensions.kt
@@ -2,9 +2,10 @@ package com.egorblagochinnov.validators
 
 import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-
+import java.lang.ref.WeakReference
 
 fun <T> Condition<T>.isValid(value: T): Boolean {
     return this.validate(value).isValid
@@ -18,4 +19,49 @@ fun TextView.textChanges(): LiveData<String?> {
     }
 
     return result
+}
+
+/**
+ * Подключает валидатор [TextViewValidatorObserver] к [TextView]
+ * Подписывается на валидатор (активирует его)
+ * Следит за фокусом [TextView]
+ *
+ * @param lifecycleOwner - [LifecycleOwner] Нужен для подписки на валидатор [LiveDataValidator]
+ * @param validator - Валидатор, по которому проверяется поле
+ *
+ * @return готовый [TextViewValidatorObserver]
+ * **/
+fun <D> TextView.validateBy(
+        lifecycleOwner: LifecycleOwner,
+        validator: LiveDataValidator<D>
+): TextViewValidatorObserver<D> {
+    val viewValidator = TextViewValidatorObserver(WeakReference(this), validator)
+    viewValidator.activate(lifecycleOwner)
+    viewValidator.setAsFocusListener()
+    return viewValidator
+}
+
+/**
+ * Создаёт условие проверки и подключает его к [TextView]
+ *
+ * @return готовый [TextViewValidator]
+ * **/
+fun TextView.validateBy(
+    errorMessage: String,
+    isValueValid: (value: CharSequence?) -> Boolean
+): TextViewValidator<TextView> {
+    return validateBy(Condition.create(errorMessage, isValueValid))
+}
+
+/**
+ * Подключает условие валидности к [TextView]
+ *
+ * @return готовый [TextViewValidator]
+ * **/
+fun TextView.validateBy(
+    condition: Condition<CharSequence?>
+): TextViewValidator<TextView> {
+    val viewValidator = TextViewValidator(WeakReference(this), condition)
+    viewValidator.activate()
+    return viewValidator
 }
